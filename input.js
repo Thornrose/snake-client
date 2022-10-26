12a7466e4389055cdb6031600c369f4b988f0437
Cheever Esler <ames.esler@gmail.com>
2022-10-26 19:32:10 -0300

    we have movement

--- input.js
+++ input.js
@@ -1,5 +1,11 @@
+
+
+// shoehorning connection - "Stores the active TCP connection object"
+let connection;
+
 // setting up interface to handle user input from stdin
-const setupInput = () => {
+const setupInput = (conn) => {
+  connection = conn;
   const stdin = process.stdin;
 
 
@@ -12,8 +18,25 @@ const setupInput = () => {
   return stdin;
 };
 
-const handleUserInput = () => { // could also have (key) and then if (key ===...) - might be easier to read
-  if (`\u0003`){
+const handleUserInput = (key) => {  // could also have (key) and then if (key ===...) - might be easier to read
+
+  if (key === "w") {                // UPDATE: above is wrong, need (key)
+    connection.write("Move: up");
+  }
+
+  if (key === "a") {
+    connection.write("Move: left");
+  }
+
+  if (key === "d") {
+    connection.write("Move: right");
+  }
+
+  if (key === "s") {
+    connection.write("Move: down");
+  }
+
+  if (key === `\u0003`){
     process.exit();
   }
 }
--- play.js
+++ play.js
@@ -1,10 +1,11 @@
 // const net = require("net");
 
+const { builtinModules } = require("module");
 const { connect } = require("./client");
 const { setupInput } = require("./input");
 
 
 console.log("Connecting ...");
-connect();
 
-setupInput();
+
+setupInput(connect());
