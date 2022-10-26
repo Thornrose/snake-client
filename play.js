499bc45b52882ec739f5defe4727153b5145cd0e
Cheever Esler <ames.esler@gmail.com>
2022-10-26 16:52:16 -0300

    starting to setup input

--- play.js
+++ play.js
@@ -1,5 +1,6 @@
 // const net = require("net");
 
+const { stdout } = require("process");
 const { connect } = require("./client");
 
 
@@ -7,3 +8,26 @@ console.log("Connecting ...");
 connect();
 
 // setting up interface to handle user input from stdin
+
+const setupInput = () => {
+  const stdin = process.stdin;
+
+
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+
+  stdin.on("data", handleUserInput);
+
+  return stdin;
+};
+
+const handleUserInput = () => { // could also have (key) and then if (key ===...) - might be easier to read
+  if (`\u0003`){
+    process.exit();
+  }
+}
+
+setupInput();
+
+// going to need `\u0003` for ctrl+C
