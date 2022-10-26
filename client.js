1ee51bba2ebc5c34d28fbf00107107331b7a78a3
Cheever Esler <ames.esler@gmail.com>
2022-10-26 15:27:11 -0300

    separated client and play files

---
+++ client.js
@@ -0,0 +1,23 @@
+const net = require("net");
+
+const connect = () => {
+  const conn = net.createConnection({
+    host: "165.227.47.243", // IP address
+    port: 50541
+  });
+
+  //interpret incoming data as plain text
+  conn.setEncoding("utf8");
+
+  conn.on("connect", () => {
+    console.log("-connected-")
+  });
+
+  conn.on("data", (data) => {
+    console.log(data);
+  })
+
+  return conn;
+};
+
+module.exports = { connect };
--- play.js
+++ play.js
@@ -1,25 +1,6 @@
-const net = require("net");
 
-// establishing a connection with the game server
-const connect = () => {
-  const conn = net.createConnection({
-    host: "165.227.47.243", // IP address
-    port: 50541
-  });
+const { connect } = require("./client");
 
-  //interpret incoming data as plain text
-  conn.setEncoding("utf8");
-
-  conn.on("connect", () => {
-    console.log("-connected-")
-  });
-
-  conn.on("data", (data) => {
-    console.log(data);
-  })
-
-  return conn;
-};
 
 console.log("Connecting ...");
 connect();
